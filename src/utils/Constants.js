e7bc4b960ffeeca460011d40512cd6c5476961dd
Rasheed <a.rasheedalabi@gmail.com>
2021-05-16 07:50:03 +0100

    pass baseUrl as env

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -2,6 +2,8 @@
 // export const BASE_URL = "http://167.172.220.53/api/app";
 // export const BILLING_URL = "http://167.172.220.53/api/payment";
 
-export const AUTH_URL = "https://odfs.app/api/auth";
-export const BASE_URL = "https://odfs.app/api/app";
-export const BILLING_URL = "https://odfs.app/api/payment";
+export const apiBaseUrl = process.env.API_BASE_URL;
+
+export const AUTH_URL = `${apiBaseUrl}/auth`;
+export const BASE_URL = `${apiBaseUrl}/app`;
+export const BILLING_URL = `${apiBaseUrl}/payment`;
