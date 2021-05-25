8ade02a25df1abf70e8e6c32b0bd1cfaae0aeade
mpaccione <mpaccione1991@gmail.com>
2021-05-24 21:29:10 -0700

    Fixed Calculate on Credit Application

--- src/pages/credit/CreditAppPage.jsx
+++ src/pages/credit/CreditAppPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Container, Icon, Segment, Loader } from "semantic-ui-react";
+import { Container, Segment, Loader } from "semantic-ui-react";
 import queryString from "query-string";
 import { numbers } from "./CreditAppConstants";
 import PersonalForm from "./components/mainComponents/personalForm/PersonalForm";
